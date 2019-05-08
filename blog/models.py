404042f4f7e5b008710ec2e71ea43f1b280fb52b
fuji <littleriddle0301@gmail.com>
2019-05-08 15:30:27 +0900

    Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete = models.CASCADE)
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default = timezone.now)
+    published_date = models.DateTimeField(blank = True, null = True)
+
+    def publish(self):
+        self.pablished_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+'''
+models.CharField : 文字数が制限されたテキストを定義するフィールド
+models.TextField : 制限なしの長いテキスト用
+models.DateTimeField : 日付と時間のフィールド
+models.foreignKey : 他のモデルへのリンク
+'''
